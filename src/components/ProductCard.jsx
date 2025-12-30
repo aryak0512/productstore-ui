3f33a69c3b515746c397d16335d442b29851273a
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-12-30 14:20:05 +0530

    Tailwind CSS added for components

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,18 +2,24 @@ import Price from "./Price.jsx";
 
 const ProductCard = ({product}) => {
     return (
-        <>
+        <div className="w-72 rounded-md mx-auto border border-gray-300 shadow-md overflow-hidden
+            flex flex-col bg-white hover:shadow-lg transition">
             <div>
-                <img
-                    src={product.imageUrl}
-                    alt={product.name}
-                    className="product-card-image"
-                />
+                <img src={product.imageUrl} alt={product.name}
+                     className="w-full h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"/>
             </div>
-            <h1>{product.name}</h1>
-            <p>{product.description}</p>
-            <Price price={product.price} currency="$"/>
-        </>
+            <div className="relative h-48 p-4 flex flex-col font-primary">
+                <h2 className="text-xl font-semibold text-primary mb-2">
+                    {product.name}
+                </h2>
+                <p className="text-base text-gray-600 mb-4">{product.description}</p>
+                <div className="flex items-center justify-between mt-auto">
+                    <div className="bg-lighter text-primary font-medium text-sm py-2 px-4 rounded-tl-md">
+                        <Price currency="$" price={product.price}/>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     );
 }
--- src/components/ProductListings.jsx
+++ src/components/ProductListings.jsx
@@ -3,17 +3,18 @@ import ProductCard from "./ProductCard.jsx";
 const ProductListings = ({products}) => {
     console.log("Products", products)
     return (
-        <>
-            {/*tricky nasty syntax alert!!*/}
-            {products.length > 0 ? (
-                products.map((product) => (
-                    <ProductCard key={product.productId} product={product}/>
-                ))
-            ) : (
-                <p className="product-listings-empty">No products found</p>
-            )}
-        </>
-
+        <div className="max-w-[1152px] mx-auto">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
+                {/*tricky nasty syntax alert!!*/}
+                {products.length > 0 ? (
+                    products.map((product) => (
+                        <ProductCard key={product.productId} product={product}/>
+                    ))
+                ) : (
+                    <p className="text-center font-primary font-bold text-lg text-primary">No products found</p>
+                )}
+            </div>
+        </div>
     );
 }
 
