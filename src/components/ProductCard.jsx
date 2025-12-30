7433b471bb66c37e34530f01a039a33e67013030
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-12-30 13:00:00 +0530

    Products image URL added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,6 +3,13 @@ import Price from "./Price.jsx";
 const ProductCard = ({product}) => {
     return (
         <>
+            <div>
+                <img
+                    src={product.imageUrl}
+                    alt={product.name}
+                    className="product-card-image"
+                />
+            </div>
             <h1>{product.name}</h1>
             <p>{product.description}</p>
             <Price price={product.price} currency="$"/>
