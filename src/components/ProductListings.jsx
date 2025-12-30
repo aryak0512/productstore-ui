f1be612cef21901520664cf3180e6e62908cfec0
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-12-30 19:00:56 +0530

    Searchbar implemented

--- src/components/ProductListings.jsx
+++ src/components/ProductListings.jsx
@@ -1,13 +1,31 @@
 import ProductCard from "./ProductCard.jsx";
+import SearchBox from "./SearchBox.jsx";
+import {useState} from "react";
 
 const ProductListings = ({products}) => {
-    console.log("Products", products)
+
+    const [itemToSearch, setItemToSearch] = useState("")
+
+    // used by SearchBox
+    function handleSearchChange(searchText) {
+        setItemToSearch(searchText);
+        console.log("Text given for search is : ", itemToSearch)
+    }
+
+    const filteredProducts = products.filter(product =>
+        product.name.toLowerCase().includes(itemToSearch.toLowerCase())
+    );
+
     return (
         <div className="max-w-[1152px] mx-auto">
+
+            <SearchBox label="Search" placeholder="Seach products..." value={itemToSearch}
+                       handleSearchChange={handleSearchChange}/>
+
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
                 {/*tricky nasty syntax alert!!*/}
-                {products.length > 0 ? (
-                    products.map((product) => (
+                {filteredProducts.length > 0 ? (
+                    filteredProducts.map((product) => (
                         <ProductCard key={product.productId} product={product}/>
                     ))
                 ) : (
