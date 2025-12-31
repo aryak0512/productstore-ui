ac1eb438b592e5920c92716074d2fe6ac037ae34
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-12-31 17:32:23 +0530

    Routing syntax simplified

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import Login from "./components/Login.jsx";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
@@ -9,40 +9,19 @@ import Cart from "./components/Cart.jsx";
 import Home from "./components/Home.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
 
-export const appRouter = createBrowserRouter([
+const routeDefinitions = createRoutesFromElements(
+    <Route path="/" element={<App/>} errorElement={<ErrorPage/>}>
+        <Route index element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+    </Route>
+);
 
-    {
-        path: "/",
-        element: <App/>,
-        errorElement: <ErrorPage/>,
-        children: [
-            {
-                index: true,
-                element: <Home/>   // App becomes base
-            },
-            {
-                path: "/home",
-                element: <Home/>
-            },
-            {
-                path: "/login",
-                element: <Login/>
-            },
-            {
-                path: "/about",
-                element: <About/>
-            },
-            {
-                path: "/contact",
-                element: <Contact/>
-            },
-            {
-                path: "/cart",
-                element: <Cart/>
-            }
-        ]
-    }
-]);
+const appRouter = createBrowserRouter(routeDefinitions);
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <RouterProvider router={appRouter}></RouterProvider>
